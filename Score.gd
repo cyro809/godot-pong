2795b8c7862e2f22da05a08811d6b694ce684da9
Cyro Guimaraes <cyro809@gmail.com>
2023-07-09 18:17:35 -0300

    Emit signal when p1 or p2 wins

--- Score.gd
+++ Score.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 var p1_score = 0
 var p2_score = 0
+var MAX_SCORE = 3
 onready var ball = get_tree().current_scene.get_node("Ball")
+signal game_over_signal(winner_player)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,4 +19,13 @@ func _add_score_player(goal_name: String):
 	elif goal_name == "Player2Goal":
 		p1_score += 1
 		$Player1Score.text = String(p1_score)
+	_is_game_over()
 
+
+func _is_game_over():
+	if p1_score >= MAX_SCORE:
+		emit_signal("game_over_signal", "player1")
+		print("Player 1 WiN!")
+	elif p2_score >= MAX_SCORE:
+		emit_signal("game_over_signal", "player2")
+		print("Player 2 WiN!")
