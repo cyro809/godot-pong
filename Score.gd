0fe9cfc292a5f383d882b7220cf64dc6971c0091
Cyro Guimaraes <cyro809@gmail.com>
2023-08-05 22:34:30 -0300

    Restarting game after max score

--- Score.gd
+++ Score.gd
@@ -4,6 +4,7 @@ var p1_score = 0
 var p2_score = 0
 var MAX_SCORE = 3
 onready var ball = get_tree().current_scene.get_node("Ball")
+onready var win_dialog = get_tree().current_scene.get_node("AcceptDialog")
 signal game_over_signal(winner_player)
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +13,8 @@ func _ready():
 	$Player2Score.text = String(p2_score)
 	ball.connect("score_signal", self, "_add_score_player")
 	Globals.score_node = self
+	win_dialog.connect("confirmed", self, "_restart_game")
+	
 
 func _add_score_player(goal_name: String):
 	if goal_name == "Player1Goal":
@@ -26,7 +29,13 @@ func _add_score_player(goal_name: String):
 func _is_game_over():
 	if p1_score >= MAX_SCORE:
 		emit_signal("game_over_signal", "player1")
+		get_tree().paused = true
 		print("Player 1 WiN!")
 	elif p2_score >= MAX_SCORE:
 		emit_signal("game_over_signal", "player2")
+		get_tree().paused = true
 		print("Player 2 WiN!")
+		
+func _restart_game():
+	get_tree().reload_current_scene()
+	get_tree().paused = false
--- globals.gd
+++ globals.gd
@@ -4,6 +4,7 @@ extends Node
 # Declare member variables here. Examples:
 var ball_node = null;
 var score_node = null;
+var win_dialog = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
