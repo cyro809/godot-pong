ba614077b040713dbfb0a1949fce6a04bd4c93e1
Cyro Guimaraes <cyro809@gmail.com>
2023-08-12 19:13:56 -0300

    Adds simple ai for player 2

--- Player2.gd
+++ Player2.gd
@@ -6,7 +6,8 @@ extends KinematicBody2D
 # var b = "text"
 var my_position_x = null
 var speed = Vector2(0, 0)
-
+onready var ball = get_tree().current_scene.get_node("Ball")
+var ai_control = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,15 +17,35 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("p2_move_down"):
-		speed.y = 100
-	elif Input.is_action_pressed("p2_move_up"):
-		speed.y = -100
+	if Input.is_action_just_pressed("toggle_p2"):
+		ai_control = not ai_control
+		
+	if ai_control:
+		self._ai_movement()
 	else:
-		speed.y = 0
+		if Input.is_action_pressed("p2_move_down"):
+			speed.y = 100
+		elif Input.is_action_pressed("p2_move_up"):
+			speed.y = -100
+		else:
+			speed.y = 0
 #	position.x = my_position_x
 	var collision = move_and_collide(speed * delta)
 	if collision != null:
 		if collision.collider.name == "Ball":
 			Globals.ball_node.velocity.x = 180
-			print(collision.collider.name)
+
+
+func _ai_movement():
+	speed.y = self._ai_speed_on_ball_position()
+	
+func _ai_speed_on_ball_position():
+	var ball_position = ball.global_position.y
+	var ball_height = ball.get_node("Sprite").texture.get_height()
+	
+	if ball_position + ball_height/2 > global_position.y + get_node("AnimatedSprite").frames.get_frame("default", 0).get_height()/2:
+		return 100
+	elif ball_position + ball_height/2 < global_position.y + get_node("AnimatedSprite").frames.get_frame("default", 0).get_height()/2:
+		return -100
+#	else:
+#		return 0
