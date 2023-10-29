f9170d7607c8559e08c96dfe6bd53ac5bc214e5e
Cyro Guimaraes <cyro809@gmail.com>
2023-10-29 18:45:11 -0300

    WIP: Pause

---
+++ ContinueButton.gd
@@ -0,0 +1,15 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("pressed", self, "_resume_game")
+
+func _resume_game():
+	get_tree().paused = false
+	get_parent().visible = false
--- WinnerCanva.gd
+++ WinnerCanva.gd
@@ -10,7 +10,15 @@ func _ready():
 
 func _show_winner_popup(winner_player: String):
 	if winner_player == "player1":
-		self.get_node("Winner").bbcode_text =  "[center]%s[/center]" % "Player 1\nWins!"
+		self.get_node("CanvaText").bbcode_text = "[center]%s[/center]" % "Player 1\nWins!"
 	elif winner_player == "player2":
-		self.get_node("Winner").bbcode_text = "[center]%s[/center]" % "Player 2\nWins!"
+		self.get_node("CanvaText").bbcode_text = "[center]%s[/center]" % "Player 2\nWins!"
 	self.visible = true
+	
+func _show_pause_menu():
+	self.get_node("CanvaText").bbcode_text = "[center]%s[/center]" % "Paused"
+	self.visible = true
+
+func _process(delta):
+	if Input.is_action_pressed("pause"):
+		self._show_pause_menu()
